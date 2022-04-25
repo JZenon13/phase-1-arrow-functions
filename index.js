443afbc1c3330341dd55beb51d13ae962d225f44
jesse zenon <jesse.zenon@gmail.com>
2022-04-25 15:07:48 -0500

    commit tree c55bf06f5aa9fe0135015528ed0ea874bf0e5e78

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const divide = () => 2000 / 100;
+// const divide = () => 2000 / 100;
 
-const square = (num1) => num1 * num1;
+// const square = (num1) => num1 * num1;
 
-const add = (num3, num4) => num3 + num4;
+// const add = (num3, num4) => num3 + num4;
+
+const divide = () => {
+  return 2000 / 100;
+};
+
+let square = (num) => {
+  return num * num;
+};
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
